7a3fb7f1fa6895c585e6ec557e813eb460c574a4
RajbanshiBittu <bitturajbanshi767@gmail.com>
2026-06-23 14:46:05 +0530

    add .env file

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,22 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-// Interceptor to inject the token into every single request automatically
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 export default API;
